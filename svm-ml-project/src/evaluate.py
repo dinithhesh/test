0aa465f26a1dbf964988e5032dece944cabb4cc2
dinithhesh <dheshan636@gmail.com>
2026-01-14 23:06:36 +0530

    Update dataset file for evaluation script

--- svm-ml-project/src/evaluate.py
+++ svm-ml-project/src/evaluate.py
@@ -6,10 +6,11 @@ from sklearn.metrics import classification_report
 model = joblib.load('svm_model.pkl')
 
 # Load data
-data = pd.read_csv('data/dataset.csv')
+data = pd.read_csv('data/colombo_flood_with_probabilities.csv')
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 
 # Predictions
 y_pred = model.predict(X)
 print(classification_report(y, y_pred))
+
